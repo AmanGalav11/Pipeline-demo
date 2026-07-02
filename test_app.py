a784f29b5de7370b78b1481bf6edd9084d6d94ff
AmanGalav11 <157770023+AmanGalav11@users.noreply.github.com>
2026-07-02 20:46:23 +0530

    Fix expected output in welcome message test

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 from app import welcome_message
 
 def test_welcome_message():
-    assert welcome_message() == "Welcome to CI/CD Pipeline"
+    assert welcome_message() == "Welcome to CI/CD Pipelin"
 
